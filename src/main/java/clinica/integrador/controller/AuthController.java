df5e339de31334b268eb64c9058c3e5e6711c0ab
isakawecki <isakawecki@gmail.com>
2025-06-10 11:01:53 -0300

    security

--- src/main/java/clinica/integrador/controller/AuthController.java
+++ src/main/java/clinica/integrador/controller/AuthController.java
@@ -1,5 +1,32 @@
 package clinica.integrador.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import clinica.integrador.dto.LoginDTO;
+import clinica.integrador.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
+        try {
+            String token = authService.autenticarEGerarToken(loginDTO);
+
+            return ResponseEntity.ok().body("{\"token\": \"" + token + "\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+        }
+    }
 
 }
