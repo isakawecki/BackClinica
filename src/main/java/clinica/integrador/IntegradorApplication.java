dd84ad2ee7afad412499a7e735579d3c5bc9acfb
isakawecki <isakawecki@gmail.com>
2025-06-17 10:51:08 -0300

    correção de erros

--- src/main/java/clinica/integrador/IntegradorApplication.java
+++ src/main/java/clinica/integrador/IntegradorApplication.java
@@ -2,12 +2,15 @@ package clinica.integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "clinica.integrador.entities")
+@EnableJpaRepositories(basePackages = "clinica.integrador.repository")
 public class IntegradorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorApplication.class, args);
 	}
-
 }
